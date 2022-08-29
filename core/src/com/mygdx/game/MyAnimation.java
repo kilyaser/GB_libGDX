4f12746e63755dd7a7dc802fa57b72c3b150b590
Leva <piknik0711@yandex.ru>
2022-08-29 17:51:43 +0300

    change animation

--- core/src/com/mygdx/game/MyAnimation.java
+++ core/src/com/mygdx/game/MyAnimation.java
@@ -3,32 +3,36 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyAnimation {
     private Texture img;
+    private TextureAtlas atlas;
     private Animation<TextureRegion> animation;
     private float time;
-    private int xPosition = 0;
-    private int yPosition = 0;
 
-    public MyAnimation(String name, int col, int row, Animation.PlayMode playMode){
-        img = new Texture(name);
-        TextureRegion region0 = new TextureRegion(img);
-        int xCnt = region0.getRegionWidth() / col;
-        int yCnt = region0.getRegionHeight() / row;
-        TextureRegion[][] regions0 = region0.split(xCnt, yCnt);
-        TextureRegion[] regions = new TextureRegion[regions0.length * regions0[0].length];
-        int cnt = 0;
-        for(int i = 0; i < regions0.length; i++){
-            for(int j = 0; j < regions0[0].length; j++){
-                regions[cnt++] = regions0[i][j];
-            }
-        }
-        animation = new Animation<TextureRegion>(1f/30f, regions);
+//    private int xPosition = 0;
+//    private int yPosition = 0;
+
+    public MyAnimation(String name, Animation.PlayMode playMode){
+//        img = new Texture(name);
+//        TextureRegion region0 = new TextureRegion(img);
+//        int xCnt = region0.getRegionWidth() / col;
+//        int yCnt = region0.getRegionHeight() / row;
+//        TextureRegion[][] regions0 = region0.split(xCnt, yCnt);
+//        TextureRegion[] regions = new TextureRegion[regions0.length * regions0[0].length];
+//        int cnt = 0;
+//        for(int i = 0; i < regions0.length; i++){
+//            for(int j = 0; j < regions0[0].length; j++){
+//                regions[cnt++] = regions0[i][j];
+//            }
+//        }
+        atlas = new TextureAtlas("atlas/vervolfTS.atlas");
+        animation = new Animation<TextureRegion>(1f/15, atlas.findRegions(name));
         animation.setPlayMode(playMode);
 
-        time += Gdx.graphics.getDeltaTime();
+        time = 0;
 
     }
 
@@ -48,17 +52,18 @@ public class MyAnimation {
     }
     public void dispose(){
         img.dispose();
+        atlas.dispose();
     }
-    public int getX(){
-        return xPosition;
-    }
-    public  int getY(){
-        return yPosition;
-    }
-    public void incrementX(){
-        this.xPosition++;
-    }
-    public void decrementX(){
-        this.xPosition--;
-    }
+//    public int getX(){
+//        return xPosition;
+//    }
+//    public  int getY(){
+//        return yPosition;
+//    }
+//    public void incrementX(){
+//        this.xPosition++;
+//    }
+//    public void decrementX(){
+//        this.xPosition--;
+//    }
 }
--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -16,6 +17,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Main;
+import com.mygdx.game.MyAnimation;
 
 
 public class GameScreen implements Screen {
@@ -26,16 +28,19 @@ public class GameScreen implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
     private float STEP = 12;
-    private Rectangle mapSize;
-    private ShapeRenderer shapeRenderer;
-    private Rectangle exitRect;
-    private TextureAtlas atlasQuit;
+    private float time;
+    MyAnimation animation;
+//    private Rectangle mapSize;
+//    private ShapeRenderer shapeRenderer;
+//    private Rectangle exitRect;
+//    private TextureAtlas atlasQuit;
 
     public GameScreen(Main game) {
         this.game = game;
         batch = new SpriteBatch();
-        shapeRenderer = new ShapeRenderer();
+//        shapeRenderer = new ShapeRenderer();
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        animation = new MyAnimation("stay", Animation.PlayMode.LOOP);
 
         map = new TmxMapLoader().load("map/map_1.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(map);
@@ -43,10 +48,10 @@ public class GameScreen implements Screen {
         RectangleMapObject tmp = (RectangleMapObject) map.getLayers().get("camera_point").getObjects().get("camera");
         camera.position.x = tmp.getRectangle().x;
         camera.position.y = tmp.getRectangle().y;
-        tmp = (RectangleMapObject) map.getLayers().get("camera_point").getObjects().get("border");
-        mapSize = tmp.getRectangle();
-        atlasQuit = new TextureAtlas("atlas/unnamed.atlas");
-        exitRect = new Rectangle(0, mapSize.height -atlasQuit.findRegion("quit").getRegionHeight(), 100, 50);
+//        tmp = (RectangleMapObject) map.getLayers().get("camera_point").getObjects().get("border");
+//        mapSize = tmp.getRectangle();
+//        atlasQuit = new TextureAtlas("atlas/unnamed.atlas");
+//        exitRect = new Rectangle(0, mapSize.height -atlasQuit.findRegion("quit").getRegionHeight(), 100, 50);
     }
 
     @Override
@@ -56,38 +61,40 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && mapSize.x < camera.position.x - 1) camera.position.x -= STEP;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && mapSize.x + mapSize.width > camera.position.x + 1) camera.position.x += STEP;
-        if (Gdx.input.isKeyPressed(Input.Keys.UP) && mapSize.y < camera.position.y - 1) camera.position.y += STEP;
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && mapSize.y - mapSize.height > camera.position.x - 1) camera.position.y -= STEP;
-
+//        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && mapSize.x < camera.position.x - 1) camera.position.x -= STEP;
+//        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && mapSize.x + mapSize.width > camera.position.x + 1) camera.position.x += STEP;
+//        if (Gdx.input.isKeyPressed(Input.Keys.UP) && mapSize.y < camera.position.y - 1) camera.position.y += STEP;
+//        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && mapSize.y - mapSize.height > camera.position.x - 1) camera.position.y -= STEP;
+        time += Gdx.graphics.getDeltaTime();
+        animation.setTime(time);
         if(Gdx.input.isKeyPressed(Input.Keys.P)) camera.zoom += 0.1f;
         if(Gdx.input.isKeyPressed(Input.Keys.O) && camera.zoom > 0) camera.zoom -= 0.1f;
         camera.update();
         ScreenUtils.clear(Color.BROWN);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(atlasQuit.findRegion("quit"), exitRect.getX(), exitRect.getY(), exitRect.getWidth(), exitRect.getHeight());
+        batch.draw(animation.getFrame(), camera.position.x, camera.position.y);
+//        batch.draw(atlasQuit.findRegion("quit"), exitRect.getX(), exitRect.getY(), exitRect.getWidth(), exitRect.getHeight());
         batch.end();
         mapRenderer.setView(camera);
         mapRenderer.render();
 
-        shapeRenderer.setProjectionMatrix(camera.combined);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(Color.GOLD);
-        shapeRenderer.rect(mapSize.x, mapSize.y, mapSize.width, mapSize.height);
-        shapeRenderer.rect(exitRect.x, exitRect.y, exitRect.width, exitRect.height);
-        shapeRenderer.end();
-
-        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-            int x = Gdx.input.getX();
-            int y = Gdx.graphics.getHeight() - Gdx.input.getY();
-            System.out.println("contains: " + exitRect.contains(x, y));
-            if (exitRect.contains(x, y)) {
-                dispose();
-                game.setScreen(new MenuScreen(game));
-            }
-        }
+//        shapeRenderer.setProjectionMatrix(camera.combined);
+//        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+//        shapeRenderer.setColor(Color.GOLD);
+//        shapeRenderer.rect(mapSize.x, mapSize.y, mapSize.width, mapSize.height);
+//        shapeRenderer.rect(exitRect.x, exitRect.y, exitRect.width, exitRect.height);
+//        shapeRenderer.end();
+//
+//        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+//            int x = Gdx.input.getX();
+//            int y = Gdx.graphics.getHeight() - Gdx.input.getY();
+//            System.out.println("contains: " + exitRect.contains(x, y));
+//            if (exitRect.contains(x, y)) {
+//                dispose();
+//                game.setScreen(new MenuScreen(game));
+//            }
+//        }
 
 
     }
@@ -117,8 +124,9 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         this.batch.dispose();
-        this.shapeRenderer.dispose();
-        this.atlasQuit.dispose();
+        this.animation.dispose();
+//        this.shapeRenderer.dispose();
+//        this.atlasQuit.dispose();
 
     }
 }
